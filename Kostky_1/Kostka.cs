37cf4fc393308aefaeaf98e1ef73970742e91588
Mirek Stejskal <Stejskal.Mirek@stud.vassboskovice.cz>
2022-04-05 11:34:08 +0200

    Add project files.

---
+++ Kostky_1/Kostka.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kostky_1
+{
+    public class Kostka
+    {
+        private int hodnota;
+        public int Hodnota { get => hodnota; }
+
+        public Kostka()
+        {
+            Hod();
+        }
+        public void Hod()
+        {
+            Random random = new Random();
+            hodnota = random.Next(1, 7);
+
+        }
+    }
+}
