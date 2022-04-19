0a26fb66cc6a52017adaaa619736621139f7bd87
Mirek Stejskal <Stejskal.Mirek@stud.vassboskovice.cz>
2022-04-19 11:35:54 +0200

    123

--- Kostky_1/MainWindow.xaml.cs
+++ Kostky_1/MainWindow.xaml.cs
@@ -35,5 +35,41 @@ namespace Kostky_1
         {
             InitializeComponent();
         }
+
+        private void ZobrazKostky()
+        {
+            kostka1.Content = kostky[0].Hodnota;
+            kostka2.Content = kostky[1].Hodnota;
+            kostka3.Content = kostky[2].Hodnota;
+            kostka4.Content = kostky[3].Hodnota;
+            kostka5.Content = kostky[4].Hodnota;
+            kostka6.Content = kostky[5].Hodnota;
+        }
+
+        private void btnHod_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var kostka in kostky)
+            {
+                kostka.Hod();
+            }
+            ZobrazKostky();
+        }
+        private int SpocitejBody()
+        {
+            int body = 0;
+            Dictionary<int, int> pocty = new Dictionary<int, int>();
+            pocty.Add(1, 0);
+            pocty.Add(2, 0);
+            pocty.Add(3, 0);
+            pocty.Add(4, 0);
+            pocty.Add(5, 0);
+            pocty.Add(6, 0);
+
+            foreach (var kostka in kostky)
+            {
+                pocty[kostka.Hodnota]++;
+            }
+            return body;
+        }
     }
 }
