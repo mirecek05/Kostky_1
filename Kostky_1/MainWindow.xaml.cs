c7cd0a35482ed3e5d21a088f94c1994e486a1e1b
Mirek Stejskal <Stejskal.Mirek@stud.vassboskovice.cz>
2022-04-26 17:46:39 +0200

    uz to hazi ( za 1)

--- Kostky_1/MainWindow.xaml.cs
+++ Kostky_1/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace Kostky_1
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Kostka[] kostky = new Kostka[6];
+        //Kostka[] kostky = new Kostka[6]; - taky moznost udelat kostky
         Kostka[] kostky = new Kostka[]
         {
             new Kostka(),
@@ -39,7 +39,9 @@ namespace Kostky_1
             k1.Fill = new ImageBrush(GetImage(Properties.Resources.dice_one));
             Grid.SetRow(k1, 1);
             Grid.SetColumn(k1, 1);
+
         }
+       
         private ImageSource GetImage(byte[] resouce)
         {
             MemoryStream memoryStream = new MemoryStream(resouce);
@@ -52,11 +54,16 @@ namespace Kostky_1
         private void ZobrazKostky()
         {
             ZobrazKostku(k1, kostky[0].Hodnota);
-            kostka2.Content = kostky[1].Hodnota;
-            kostka3.Content = kostky[2].Hodnota;
-            kostka4.Content = kostky[3].Hodnota;
-            kostka5.Content = kostky[4].Hodnota;
-            kostka6.Content = kostky[5].Hodnota;
+            ZobrazKostku(k2, kostky[1].Hodnota);
+            ZobrazKostku(k3, kostky[2].Hodnota);
+            ZobrazKostku(k4, kostky[3].Hodnota);
+            ZobrazKostku(k5, kostky[4].Hodnota);
+            ZobrazKostku(k6, kostky[5].Hodnota);
+            //kostka2.Content = kostky[1].Hodnota;
+            //kostka3.Content = kostky[2].Hodnota;
+            //kostka4.Content = kostky[3].Hodnota;
+            //kostka5.Content = kostky[4].Hodnota;
+            //kostka6.Content = kostky[5].Hodnota;
 
 
         }
@@ -90,7 +97,7 @@ namespace Kostky_1
 
 
         }
-
+        
         private void btnHod_Click(object sender, RoutedEventArgs e)
         {
             foreach (var kostka in kostky)
