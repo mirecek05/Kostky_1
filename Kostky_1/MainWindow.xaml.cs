482c6aba607dcd1fb9e250c5a66bd5a3199b16b2
Mirek Stejskal <Stejskal.Mirek@stud.vassboskovice.cz>
2022-04-26 11:37:04 +0200

    26.4 po hodine

--- Kostky_1/MainWindow.xaml.cs
+++ Kostky_1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,16 +35,60 @@ namespace Kostky_1
         public MainWindow()
         {
             InitializeComponent();
+            ZobrazKostky();
+            k1.Fill = new ImageBrush(GetImage(Properties.Resources.dice_one));
+            Grid.SetRow(k1, 1);
+            Grid.SetColumn(k1, 1);
+        }
+        private ImageSource GetImage(byte[] resouce)
+        {
+            MemoryStream memoryStream = new MemoryStream(resouce);
+            BitmapFrame bitmapFrame = BitmapFrame.Create(memoryStream);
+            Image image = new Image();
+            image.Source = bitmapFrame;
+            return image.Source;
         }
 
         private void ZobrazKostky()
         {
-            kostka1.Content = kostky[0].Hodnota;
+            ZobrazKostku(k1, kostky[0].Hodnota);
             kostka2.Content = kostky[1].Hodnota;
             kostka3.Content = kostky[2].Hodnota;
             kostka4.Content = kostky[3].Hodnota;
             kostka5.Content = kostky[4].Hodnota;
             kostka6.Content = kostky[5].Hodnota;
+
+
+        }
+        private void ZobrazKostku(Rectangle rectangle, int cislo)
+        {
+            if(cislo == 1)
+            {
+             rectangle.Fill = new ImageBrush(GetImage(Properties.Resources.dice_one));
+            }
+            else if (cislo == 2)
+            {
+                rectangle.Fill = new ImageBrush(GetImage(Properties.Resources.dice_two));
+            }
+            else if (cislo == 3)
+            {
+                rectangle.Fill = new ImageBrush(GetImage(Properties.Resources.dice_three));
+            }
+            else if (cislo == 4)
+            {
+                rectangle.Fill = new ImageBrush(GetImage(Properties.Resources.dice_four));
+            }
+            else if (cislo == 5)
+            {
+                rectangle.Fill = new ImageBrush(GetImage(Properties.Resources.dice_five));
+            }
+            else if (cislo == 6)
+            {
+                rectangle.Fill = new ImageBrush(GetImage(Properties.Resources.dice_six));
+            }
+
+
+
         }
 
         private void btnHod_Click(object sender, RoutedEventArgs e)
